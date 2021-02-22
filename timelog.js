a8c507e2ac4efbfe53bb79e29132b15e5aeeb704
ChenSingYo <chensingyo@gmail.com>
2021-02-22 15:26:55 +0800

    refactor: 省略function，直接用內建的tolocalstring來新增 timeStamp

--- timelog.js
+++ timelog.js
@@ -1,19 +1,13 @@
-function timeStamp (time) {
-  const fullDate = `${time.getFullYear()}-${time.getMonth() + 1}-${time.getDate()}`
-  const fullTime = `${time.getHours()}:${time.getMinutes()}:${time.getSeconds()}`
-  const timeStamp = fullDate + ' ' + fullTime
-  return timeStamp
-}
-
 function logReqResPeriod (req, res, next) {
   const reqTime = new Date()
-  const reqTimeStamp = timeStamp(reqTime)
+  const reqTimeStamp = reqTime.toLocaleString('zh-TW', { timeZone: 'Asia/Taipei', hour12: false }).replace(/\//g, '-')
   const reqInfo = req.method + ' from ' + req.originalUrl
   console.log(reqTimeStamp + ' | ' + reqInfo)
 
   res.on('finish', () => {
     const resTime = new Date()
-    const runTimePeriod = `${timeStamp(resTime)} | ${reqInfo} | total time: ${resTime - reqTime} ms`
+    const resTimeStamp = resTime.toLocaleString('zh-TW', { timeZone: 'Asia/Taipei', hour12: false }).replace(/\//g, '-')
+    const runTimePeriod = `${resTimeStamp} | ${reqInfo} | total time: ${resTime - reqTime} ms`
     console.log(runTimePeriod)
   })
   next()
